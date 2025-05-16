4e9ae27a605e149e137e5449cdd2c4c0c90644d8
Artem Maksimov <nirendsound@gmail.com>
2025-05-16 11:42:49 +0300

    Add alembic

--- core/database.py
+++ core/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from core.config import DATABASE_URL
-from core.models import Base
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 Session = sessionmaker(bind=engine)
